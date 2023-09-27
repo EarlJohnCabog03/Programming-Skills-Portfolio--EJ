2a78b5f1718881bf9d42f8ec009ad9463ea938b6
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-09-27 21:15:32 +0400

    Update Exercise 1 print string .py

--- Chapter 1/Exercise 1 print string .py
+++ Chapter 1/Exercise 1 print string .py
@@ -9,9 +9,10 @@ Created on Tue Sep 12 14:32:36 2023
 Twinkle little starWrite a Python program to print the following string in a specific format.
 
 Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are.
+"""
 
-
-"""""
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! 
 \n\t\tUp above the world so high \n\t\tLike a diamond in the sky.\nTwinkle, 
 twinkle, little star, \n\tHow I wonder what you are!")
+"""
+"""
