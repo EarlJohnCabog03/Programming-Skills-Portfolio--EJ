81453137020bd9f54b2c8433469d34a89170ffed
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-09 10:44:45 +0400

    Add files via upload

---
+++ Chapter 3/Chapter 3_Ex3.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 10:41:11 2023
+
+@author: 97154
+"""
+
+# Define a list of motorcycle examples
+motorcycles = ["Honda", "Yamaha", "Kawasaki", "Ducati", "Harley-Davidson"]
+
+# Generate statements about the motorcycles
+for motorcycle in motorcycles:
+    print(f"I would like to own a {motorcycle} motorcycle.")
