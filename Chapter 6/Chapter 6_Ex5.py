54ff3290bccad3cab8e12428f835d9d929c14bd0
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:14:35 +0400

    Update Chapter 6_Ex5.py

--- Chapter 6/Chapter 6_Ex5.py
+++ Chapter 6/Chapter 6_Ex5.py
@@ -4,10 +4,20 @@ Created on Mon Oct  9 11:30:10 2023
 
 @author: 97154
 """
+"""
+"""
+Question Chapter 6 Exercise 5.
+#Using the list sandwich_orders from Exercise 7-8, make sure the sandwich 'pastrami' appears in the list at least three times. Add code
+#near the beginning of your program to print a message saying the deli has run out of pastrami, and then use a while loop to remove all
+#occurrences of 'pastrami' from sandwich_orders. Make sure no pastrami sandwiches end up in finished_sandwiches.
+
+
+Explanation:
 #We've added more occurrences of 'pastrami' to the sandwich_orders list.
 #We added a while loop to remove all occurrences of 'pastrami' from sandwich_orders.
 #As a result, no 'pastrami' sandwiches will end up in the finished_sandwiches list, and a message is printed to notify that the deli has run out of pastrami.
 """
+#code:
 """
 # Create a list of sandwich orders with 'pastrami' appearing at least three times
 sandwich_orders = ["tuna", "turkey", "pastrami", "vegetarian", "pastrami", "ham", "pastrami"]
