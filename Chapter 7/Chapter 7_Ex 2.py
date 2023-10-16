44fec0359a6f589ee40013631c6a026d828c4ae0
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:56:05 +0400

    Update Chapter 7_Ex 2.py

--- Chapter 7/Chapter 7_Ex 2.py
+++ Chapter 7/Chapter 7_Ex 2.py
@@ -4,8 +4,15 @@ Created on Mon Oct  9 11:36:31 2023
 
 @author: 97154
 """
+Question Chapter 7 Exercise 2.
+#Write a function called favorite_book() that accepts one parameter, title. The function should print a message, such as One of my
+#favorite books is Alice in Wonderland. Call the function, making sure to include a book title as an argument in the function call.
+"""
+"""
+Explanation:
 #You can call the favorite_book() function with any book title you like as an argument, and it will print a message accordingly.
 """
+Code:
 """
 def favorite_book(title):
     print(f"One of my favorite books is {title}.")
