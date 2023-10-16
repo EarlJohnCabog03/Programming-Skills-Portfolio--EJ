b9e1141b6849e3657ca90b07f4adc05a624d4516
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:35:01 +0400

    Update Chapter 1_Ex5.py

--- Chapter 1/Chapter 1_Ex5.py
+++ Chapter 1/Chapter 1_Ex5.py
@@ -4,7 +4,26 @@ Created on Tue Sep 12 10:47:04 2023
 
 @author: 97154
 """
+Question Chapter 1 Exercise 5.
+#Write a Python program which accepts the radius of a circle from the user and compute the area.
 
-from math import pi
-r=float(input("input the radius of the circle:"))
-print("Area",pi * r**2)     
+Explanation:
+#we first import the math module to access the constant pi. 
+#Then, we prompt the user to enter the radius, convert it to a float, calculate the area, and print the result with two decimal places.
+"""
+#Code:
+"""
+
+import math
+
+# Accept the radius from the user
+radius = float(input("Enter the radius of the circle: "))
+
+# Calculate the area of the circle
+area = math.pi * (radius**2)
+
+# Print the result
+print(f"The area of the circle with radius {radius} is {area:.2f}")
+
+"""
+"""
