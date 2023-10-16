c3c7d6003764cbb00070ed628e33d054f0e6409f
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:07:00 +0400

    Update Chapter_3 Ex4.py

--- Chapter 3/Chapter_3 Ex4.py
+++ Chapter 3/Chapter_3 Ex4.py
@@ -7,14 +7,27 @@ Created on Tue Sep 26 12:33:05 2023
 """
 Ex 4 Guest List
 """
+Question Chapter 3 Exercise 4.
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+#like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.
 
-guests = ['guido van rossum', 'jack turner', 'lynn hill']
 
-name = guests[0].title()
-print(name + ", please come to dinner.")
+Explanation:
+# A list of people you'd like to invite to dinner, and a for loop is used to print a personalized invitation message to each person in the list. 
+#You can replace the names in the list with your own choices of people you'd like to invite.
+"""
+#Code:
+"""
+"""
+"""
+# List of people to invite to dinner
+guests = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci"]
 
-name = guests[1].title()
-print(name + ", please come to dinner.")
+# Send dinner invitations
+for guest in guests:
+    invitation = f"Dear {guest}, you are cordially invited to dinner. Please join us for a delightful evening."
+    print(invitation)
 
-name = guests[2].title()
-print(name + ", please come to dinner.")
+"""
+"""
+
