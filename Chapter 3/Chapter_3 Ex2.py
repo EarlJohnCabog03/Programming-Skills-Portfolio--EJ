9f1497931452d717951e89b027d860b807318926
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:00:03 +0400

    Update Chapter_3 Ex2.py

--- Chapter 3/Chapter_3 Ex2.py
+++ Chapter 3/Chapter_3 Ex2.py
@@ -7,14 +7,23 @@ Created on Tue Sep 26 12:30:54 2023
 """
 Ex 2 Greetings
 """
+Question Chapter 3 Exercise 2.
+#Start with the list you used in Exercise 1, but instead of just
+#printing each person’s name, print a message to them. The text of each message should be the same, but each message should be
+#personalized with the person’s name.
 
-names = ['ron', 'tyler', 'dani']
+Explanation :
+#use a for loop to iterate through the names list and create a
+#personalized message for each person by including their name in the message. Each message is printed with the respective person's name.
+"""
+#Code:
+"""
+# Define a list of your friends' names
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
 
-msg = "Hello, " + names[0].title() + "!"
-print(msg)
-
-msg = "Hello, " + names[1].title() + "!"
-print(msg)
-
-msg = "Hello, " + names[2].title() + "!"
-print(msg)
+# Print a personalized message to each person
+for name in names:
+    message = f"Hello, {name}! I hope you're having a great day."
+    print(message)
+"""
+"""
