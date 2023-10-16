66ae3bb73ca683a896d5cbab15081354cb3b6591
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:29:33 +0400

    Update Chapter1_Ex3.py

--- Chapter 1/Chapter1_Ex3.py
+++ Chapter 1/Chapter1_Ex3.py
@@ -5,9 +5,20 @@ Created on Wed Sep 13 19:06:06 2023
 @author: 97154
 """
 """
-exercise 3 print date and time
-"""""
+Exercise 3 print date and time
+"""
+Question Chapter 1 Exercise 3.
+#Write a Python program to display the current date and time.
+
+
+Explanation
+#The datetime module, retrieves the current date and time using datetime.datetime.now(), and then prints the result, showing the current date and time.
+"""
+#Code:
+"""
 import datetime
 now = datetime.datetime.now()
 print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
+"""
+"""
