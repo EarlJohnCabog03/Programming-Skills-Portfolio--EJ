a22c87b22412e093a1efd35cb2b1842f80892071
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:26:34 +0400

    Update Chapter 1_Ex2.py

--- Chapter 1/Chapter 1_Ex2.py
+++ Chapter 1/Chapter 1_Ex2.py
@@ -4,6 +4,14 @@ Created on Wed Sep 13 19:19:21 2023
 
 @author: 97154
 """
+Question Chapter 1 Exercise 2.
+#Write a Python program to get the Python version you are using.
+
+Explanation:
+#It will print the Python version information, including the version number and other details.
+"""
+#Code:
+"""
 
 import sys
 print("Python version")
@@ -11,3 +19,5 @@ print (sys.version)
 
 print("Version info.")
 print (sys.version_info)
+"""
+"""
