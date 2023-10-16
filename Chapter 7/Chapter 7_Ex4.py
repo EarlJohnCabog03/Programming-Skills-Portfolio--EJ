8e925fa478763d7770475438f02ebbfc789fc7d8
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:45:51 +0400

    Update Chapter 7_Ex4.py

--- Chapter 7/Chapter 7_Ex4.py
+++ Chapter 7/Chapter 7_Ex4.py
@@ -5,9 +5,11 @@ Created on Mon Oct  9 11:42:11 2023
 @author: 97154
 """
 """
+"""
 Question chapter 7 Exercise 4.
 #Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. Make a large shirt and a
 #medium shirt with the default message, and a shirt of any size with a different message.
+"""
 """
 Explanation:
 #The default values for size and message are set to "large" and "I love Python," respectively. 
@@ -24,6 +26,8 @@ make_shirt()
 
 # Create a medium shirt with the default message
 make_shirt(size="medium")
+"""
+"""
 
 # Create a small shirt with a custom message
 make_shirt(size="small", message="Python Programmer")
