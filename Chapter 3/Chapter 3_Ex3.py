d0288bb3bf01a0c7a30c539dd5033831e25a78c2
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:03:40 +0400

    Update Chapter 3_Ex3.py

--- Chapter 3/Chapter 3_Ex3.py
+++ Chapter 3/Chapter 3_Ex3.py
@@ -4,10 +4,23 @@ Created on Mon Oct  9 10:41:11 2023
 
 @author: 97154
 """
+Question Chapter 3 Exercise 3.
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+#to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 
-# Define a list of motorcycle examples
-motorcycles = ["Honda", "Yamaha", "Kawasaki", "Ducati", "Harley-Davidson"]
+Explanation:
+# a list of favorite motorcycle brands, and a for loop is used to print a series of statements, each expressing a 
+#desire to own a motorcycle from one of these brands. You can replace the motorcycle brands in the list with your own favorites.
 
-# Generate statements about the motorcycles
-for motorcycle in motorcycles:
-    print(f"I would like to own a {motorcycle} motorcycle.")
+"""
+#Code:
+"""
+# List of favorite motorcycle brands
+motorcycle_brands = ["Honda", "Yamaha", "Kawasaki", "Suzuki", "Ducati"]
+
+# Print statements about these motorcycle brands
+for brand in motorcycle_brands:
+    statement = f"I would like to own a {brand} motorcycle."
+    print(statement)
+"""
+"""
