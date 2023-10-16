021bef71d4bafa449628c9402d0f3fc6e3165b14
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:59:30 +0400

    Update Chapter 7_Ex3.py

--- Chapter 7/Chapter 7_Ex3.py
+++ Chapter 7/Chapter 7_Ex3.py
@@ -4,7 +4,17 @@ Created on Mon Oct  9 11:39:16 2023
 
 @author: 97154
 """
+"""
+"""
+Question Chapter 7 exercise 3
+#Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function
+#should print a sentence summarizing the size of the shirt and the message printed on it. Call the function once using positional
+#arguments to make a shirt. Call the function a second time using keyword arguments.
+"""
+Explanation:
 #In the first call, we use positional arguments to specify the size and message. In the second call, we use keyword arguments to specify the size and message. Both calls will print a sentence summarizing the shirt size and message.
+"""
+#code:
 """
 """
 def make_shirt(size, message):
