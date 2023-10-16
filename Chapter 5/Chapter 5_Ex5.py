8a8090558e549c0f22e2169ba9b114082e7c64f0
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:31:13 +0400

    Update Chapter 5_Ex5.py

--- Chapter 5/Chapter 5_Ex5.py
+++ Chapter 5/Chapter 5_Ex5.py
@@ -4,9 +4,17 @@ Created on Mon Oct  9 11:14:05 2023
 
 @author: 97154
 """
+Question Chapter 5 Exercise 5.
+#Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the
+#owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and asyou do, print everything you know about
+#each pet
+
+   
+Explanation:
 #I create a list called pets containing dictionaries, where each dictionary represents a different pet with keys "kind" for the kind of animal and "owner" for the owner's name.
 #The loop iterates through the list of pets and prints information about each pet using f-strings, including the kind of animal and the owner's name. Each pet's information is separated by a newline for better readability.
 """
+#Code:
 """
 
 # Create a list of dictionaries representing different pets
