131ba1ca6682ff827bb34a8faf7177ef9349c68f
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:17:11 +0400

    Update Chapter 5_Ex1.py

--- Chapter 5/Chapter 5_Ex1.py
+++ Chapter 5/Chapter 5_Ex1.py
@@ -4,16 +4,22 @@ Created on Mon Oct  9 11:01:46 2023
 
 @author: 97154
 """
+"""
+"""
+Question Chapter 5 Exercise 1.
+#Use a dictionary to store information about a person you know.Store their first name, last name, age, and the city in which they live. You
+#should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+
+   
+Explanation:
 #In this example, we've created a dictionary called person_info with keys like
-# "first_name," "last_name," "age," and "city." Then, we print each piece of information stored in the dictionary using the corresponding keys.
+#"first_name," "last_name," "age," and "city." Then, we print each piece of information stored in the dictionary using the corresponding keys.
 #You can replace the values with the actual information about the person you know.
 """
+#code:
 """
-
-
-
-
-
+"""
+"""
 # Create a dictionary to store information about a person
 person_info = {
     "first_name": "John",
