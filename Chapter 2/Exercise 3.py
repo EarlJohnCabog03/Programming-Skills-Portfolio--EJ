70abfd6eb4965882a98e32a0139da20b374383b3
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:43:09 +0400

    Update Exercise 3.py

--- Chapter 2/Exercise 3.py
+++ Chapter 2/Exercise 3.py
@@ -4,20 +4,35 @@ Created on Tue Sep 19 09:28:11 2023
 
 @author: 97154
 """
-"""
-Exercise 3:stripping names
+Question:
+#Tidy up the code to make it easier to understand
+#Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.
+#Print the name once, so the whitespace around the name is displayed.
+#Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().
+
 
 """
-name = "\tMatthew Madrigal\n"
+Explanation.
+#This code uses the \t and \n characters for whitespace and demonstrates the effects of the lstrip(), rstrip(), and strip() 
+#functions to remove the leading, trailing, and both leading and trailing whitespace, respectively.
+"""
 
-print("Unmodified:")
+#Code:
+# Define a person's name with whitespace
+name = "\t \n   John Doe   \t \n"
+
+# Print the name with whitespace
+print("Name with Whitespace:")
 print(name)
 
-print("\nUsing lstrip():")
+# Print the name after left stripping
+print("Left Stripped Name:")
 print(name.lstrip())
 
-print("\nUsing rstrip():")
+# Print the name after right stripping
+print("Right Stripped Name:")
 print(name.rstrip())
 
-print("\nUsing strip():")
+# Print the name after stripping from both sides
+print("Stripped Name:")
 print(name.strip())
