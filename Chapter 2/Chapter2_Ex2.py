df7c5ea79cf801b9e46a88025f0bd733a853b193
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:37:48 +0400

    Update Chapter2_Ex2.py

--- Chapter 2/Chapter2_Ex2.py
+++ Chapter 2/Chapter2_Ex2.py
@@ -1,6 +1,13 @@
-print('Grand Master Yoda once said, "Do or do not')
-print('There is no try."')
+Question Chapter 2 Exercise 2
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+#Your output should look something like the following, including the quotation marks:
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.
+Explanation.
+#When you run this Python code, it will output the quote along with the author's name in the desired format.
+#Code
+"""
+"""
+quote = "A person who never made a mistake never tried anything new."
+author = "Albert Einstein"
 
-Author = ("Grand Master Yoda")
-Quote = ('"Do or do not there is no try".')
-print(Author + " once said," + Quote)
+print(f"{author} once said, '{quote}'")
