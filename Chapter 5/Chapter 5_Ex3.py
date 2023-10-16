ca5c723b5a2319a7db55e69814e6ae8ec45d3126
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:26:27 +0400

    Update Chapter 5_Ex3.py

--- Chapter 5/Chapter 5_Ex3.py
+++ Chapter 5/Chapter 5_Ex3.py
@@ -6,22 +6,37 @@ Created on Mon Oct  9 11:08:58 2023
 """
 """
 """
-
-# Create a glossary dictionary with programming terms and meanings
+Question Chapter 5 Exercise 3.
+#Now that you know how to loop through a dictionary, clean up the code from Exercise 6-3 (page 99) by replacing your series of print()
+#calls with a loop that runs through the dictionary’s keys and values. When you’re sure that your loop works, add five more Python terms
+#to your glossary.When you run your program again, these new words and meanings should automatically be included in the output.
+"""
+"""
+Explanation:
+#This code defines the original glossary and adds five more terms to it. 
+#Then, it uses a for loop to iterate through the dictionary and print each term along with its definition. Adding or modifying terms will automatically include them in the output when you run the program.
+"""
+#code:
+"""
+# Define the glossary dictionary
 glossary = {
-    "variable": "A container for storing data in a program.",
-    "function": "A reusable block of code that performs a specific task.",
-    "loop": "A control structure that repeats a block of code until a condition is met.",
-    "boolean": "A data type that represents true or false values.",
-    "algorithm": "A step-by-step set of instructions for solving a specific problem.",
-    "list": "An ordered collection of items that can be of different types.",
-    "dictionary": "A collection of key-value pairs.",
-    "tuple": "An immutable ordered collection of items.",
-    "module": "A file containing Python code that can be reused in other programs.",
-    "string": "A sequence of characters."
+    'variable': 'A container for storing data.',
+    'list': 'An ordered collection of items.',
+    'dictionary': 'A collection of key-value pairs.',
+    'function': 'A reusable block of code.',
+    'loop': 'A structure for repeating a set of instructions.',
+    'module': 'A file containing Python code.',
+    'class': 'A blueprint for creating objects.',
+    'method': 'A function associated with an object.',
+    'tuple': 'An ordered, immutable collection of items.',
+    'conditional statement': 'A decision-making structure.'
 }
 
-# Print each word and its meaning with a newline in between
-for word, meaning in glossary.items():
-    print(word + ":")
-    print(meaning + "\n")
+# Loop through the dictionary to print keys and values
+for term, definition in glossary.items():
+    print(f"{term.title()}: {definition}")
+
+# Output all the terms and their definitions
+
+
+
