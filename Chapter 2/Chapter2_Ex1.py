7156c48918e5c21ca92cfc3cafdd49c2f39e4c46
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:34:01 +0400

    Update Chapter2_Ex1.py

--- Chapter 2/Chapter2_Ex1.py
+++ Chapter 2/Chapter2_Ex1.py
@@ -1,5 +1,19 @@
+Question Chapter 2 Exercise 1
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+Explanation.
+#When you run this code, it will first print the initial message, and then it will print the new message after changing the value of the variable.
+     
+# Assign a message to a variable
+my_message = "Hello, world!"
 
-a=(input("Enter your name = "))
-b=(input("Enter your age ="))
-print("Name - ", a)
-print("Age - ", b)
+# Print the initial message
+print("Initial Message:")
+print(my_message)
+
+# Change the value of the variable to a new message
+my_message = "This is the new message."
+
+# Print the new message
+print("New Message:")
+print(my_message)
+
