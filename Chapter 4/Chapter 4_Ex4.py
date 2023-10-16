be9be977c06bc30f864b484fdf632116bb757d15
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:49:17 +0400

    Update Chapter 4_Ex4.py

--- Chapter 4/Chapter 4_Ex4.py
+++ Chapter 4/Chapter 4_Ex4.py
@@ -4,18 +4,34 @@ Created on Mon Oct  9 10:57:22 2023
 
 @author: 97154
 """
+Question Chapter 4 Exercise 4.
+#Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+#•If the person is less than 2 years old, print a message that the person is a baby.
+#•If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+#•If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+#•If the person is at least 13 years old but less than 20, print a message that the person is a teenager
+#•If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+#•If the person is age 65 or older, print a message that the person is an elder.
 
-age = 30  # You can change the age to test different scenarios
+
+
+Explanation:
+#We first get the person's age as input. Then,
+#we use a series of if-elif-else statements to check the age against the given criteria and print the appropriate message based on the age category.
+"""
+#Code:
+"""
+age = int(input("Enter the person's age: ")
 
 if age < 2:
     print("The person is a baby.")
-elif age >= 2 and age < 4:
+elif age < 4:
     print("The person is a toddler.")
-elif age >= 4 and age < 13:
+elif age < 13:
     print("The person is a kid.")
-elif age >= 13 and age < 20:
+elif age < 20:
     print("The person is a teenager.")
-elif age >= 20 and age < 65:
+elif age < 65:
     print("The person is an adult.")
 else:
     print("The person is an elder.")
