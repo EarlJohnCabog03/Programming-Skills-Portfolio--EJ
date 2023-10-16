fb8ce06430c2e825f6bbec38b071388b57c3b365
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:53:49 +0400

    Update Chapter 7_Ex1.py

--- Chapter 7/Chapter 7_Ex1.py
+++ Chapter 7/Chapter 7_Ex1.py
@@ -4,8 +4,15 @@ Created on Mon Oct  9 11:34:17 2023
 
 @author: 97154
 """
+Question chapter 7 exercise 1.
+#Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the
+#function, and make sure the message displays correctly.
+"""
+"""
+Explanation:
 #This function simply prints the message "In this chapter, I am learning about functions in Python." When you call display_message(), it will display this message.
 """
+#Code:
 """
 def display_message():
     print("In this chapter, I am learning about functions in Python.")
