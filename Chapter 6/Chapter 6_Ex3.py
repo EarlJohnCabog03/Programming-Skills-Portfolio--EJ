d45626857eafc5ca73b5c324f01cbbbbc82b1240
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 16:10:36 +0400

    Update Chapter 6_Ex3.py

--- Chapter 6/Chapter 6_Ex3.py
+++ Chapter 6/Chapter 6_Ex3.py
@@ -4,10 +4,16 @@ Created on Mon Oct  9 11:24:10 2023
 
 @author: 97154
 """
+"""
+"""
+Question Chapter 6 Exercise 3
+#Write a loop that never ends, and run it. (To end the loop, press ctrl-C or close the window displaying the output.)
+Explanation:
 #it will keep printing "This is an infinite loop!" repeatedly until you manually interrupt
 # it by pressing Ctrl-C or closing the window displaying the output, as mentioned in the prompt. 
 #Be cautious when running infinite loops, as they can consume CPU resources and may need to be terminated forcibly.
 """
+#code:
 """
 
 while True:
