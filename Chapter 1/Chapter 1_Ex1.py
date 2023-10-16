92dbf160427487c2c82084ce296b4c221c71b6bf
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 17:23:18 +0400

    Update Chapter 1_Ex1.py

--- Chapter 1/Chapter 1_Ex1.py
+++ Chapter 1/Chapter 1_Ex1.py
@@ -4,9 +4,22 @@ Created on Tue Sep 12 14:32:36 2023
 
 @author: 97154
 """
-
 """
-Twinkle little star
+"""
+Question Chapter 1Exercise 1
+#Write a Python program to print the following string in a specific format.
+#Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are
 
-"""""
-print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+Explanation:
+#prints the string "Twinkle, twinkle, little star" with the appropriate indentation to match the format you provided.
+"""
+#Code:
+"""
+print("Twinkle, twinkle, little star,")
+print("    How I wonder what you are!")
+print("        Up above the world so high,")
+print("        Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("    How I wonder what you are!")
+"""
+"""
