f24d36c6d4414c5ed783a7e56dfaec359831bd5a
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 15:46:31 +0400

    Update Exercice 4.py

--- Chapter 2/Exercice 4.py
+++ Chapter 2/Exercice 4.py
@@ -4,13 +4,26 @@ Created on Tue Sep 19 09:43:10 2023
 
 @author: 97154
 """
+Question CHapter 2 Exercise 4.
+#Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print that message.
+"""
+"""
+Explanation:
+#I've used the favorite_number variable to store my favorite number, 
+#and then I've created a message that reveals it by combining it with a string. Finally, I've printed the message to reveal my favorite number, which is 7 in this example.
 """
 
 Exercice 4: favorite number
 
+#Code:
 """
-fav_num = 26
-msg = f"My favorite number is {fav_num}."
+# Define my favorite number
+favorite_number = 7
 
-print(msg)
+# Create a message to reveal my favorite number
+message = "My favorite number is " + str(favorite_number) + "."
+
+# Print the message
+print(message)
+
 
