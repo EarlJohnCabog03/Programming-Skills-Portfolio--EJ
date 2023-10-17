d8d551c74b8587489ef5af6be7520f84b01e3db4
earl john ibanez cabog <87237883+EarlJohnCabog03@users.noreply.github.com>
2023-10-16 22:21:19 -0800

    Update Chapter2_Ex1.py

--- Chapter 2/Chapter2_Ex1.py
+++ Chapter 2/Chapter2_Ex1.py
@@ -2,11 +2,8 @@ Question Chapter 2 Exercise 1
 #Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
 Explanation.
 #When you run this code, it will first print the initial message, and then it will print the new message after changing the value of the variable.
-"""
-#Code:
-"""
-"""
-"""
+
+
 # Assign a message to a variable
 my_message = "Hello, world!"
 
